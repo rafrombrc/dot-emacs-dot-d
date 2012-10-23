32275856479aefbee4dfbfb25d36a589023c228d
Rob Miller <rob@kalistra.com>
2012-10-23 00:32:37 -0700

    more complete extending of exec path

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
 (setq exec-path (append exec-path '("/opt/local/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/opt/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'hyper)
 
