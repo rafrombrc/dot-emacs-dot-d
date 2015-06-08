06fad87aa2d9fa6e8edd8f7b424bc228f8db1bbd
Rob Miller <rob@kalistra.com>
2015-06-08 14:54:58 -0700

    Added lua mode.

--- init.el
+++ init.el
@@ -104,7 +104,9 @@ by using nxml's indentation rules."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (neotree tabbar revive go-eldoc git-gutter dirtree)))
+ '(package-selected-packages
+   (quote
+    (lua-mode neotree tabbar revive go-eldoc git-gutter dirtree)))
  '(safe-local-variable-values (quote ((encoding . utf8))))
  '(save-place t nil (saveplace))
  '(show-paren-mode t)
