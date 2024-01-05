e2d1338c545a88797c40696a76f5bc03465ea810
Rob Miller <rob@kalistra.com>
2024-01-05 13:46:56 -0800

    ignore `elpa` dir and add recent tweaks

--- elpa/flymake-lua-20140310.230/flymake-lua-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flymake-lua" "20140310.230" "Flymake for Lua" 'nil :keywords '("lua"))
--- elpa/tree-mode-20150227.216/tree-mode-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; tree-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (tree-minor-mode) "tree-mode" "tree-mode.el" (21841
-;;;;;;  5070 45348 889000))
-;;; Generated autoloads from tree-mode.el
-
-(autoload 'tree-minor-mode "tree-mode" "\
-More keybindings for tree-widget.
-
-\\{tree-mode-map}
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("tree-mode-pkg.el") (21841 5070 64132
-;;;;;;  884000))
-
-;;;***
-
-(provide 'tree-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; tree-mode-autoloads.el ends here
