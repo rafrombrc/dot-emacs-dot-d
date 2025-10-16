4f2ee60ab5c388f4157980b18e9527e836abdf2f
Rob Miller <rob@nonsequitarian.org>
2025-10-16 11:48:12 -0700

    corfu tuning

--- init.el
+++ init.el
@@ -78,30 +78,16 @@
   :config (eglot-booster-mode))
 
 (use-package corfu
-  ;; Optional customizations
-  ;; :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-
-  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
   :init
-
-  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
-  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
-  ;; variable `global-corfu-modes' to exclude certain modes.
   (global-corfu-mode)
+  (corfu-popupinfo-mode)
 
-  ;; Enable optional extension modes:
-  ;; (corfu-history-mode)
-  ;; (corfu-popupinfo-mode)
+  :bind
+  (:map corfu-map
+	("<tab>" . corfu-next))
+
+  :custom
+  (corfu-cycle t)                   ;; Enable cycling for `corfu-next/previous'
   )
 
 (setq history-length 250)
@@ -113,6 +99,7 @@
 (global-set-key "\C-]" 'help-for-help)
 (global-set-key (kbd "C-/") 'comment-line)
 (global-set-key (kbd "ESC C-<tab>") 'completion-at-point)
+(global-set-key (kbd "C-M-<tab>") 'completion-at-point)
 
 (global-auto-revert-mode)
 
